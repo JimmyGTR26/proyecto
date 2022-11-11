a7348513460bf726deffb9e4e2901a6454db4f90
JimmyGTR26 <santiago.mosler26@gmail.com>
2022-11-11 13:14:23 -0300

    archivos nuevos

---
+++ ParteA.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ParteA is
+    Port ( sw1 : in STD_LOGIC;
+	        sw2 : in STD_LOGIC;
+			  LED : out STD_LOGIC;)
+end ej_combinacional;
+
+architecture Behavioral of ParteA is
+
+
+begin
+
+LED <= not (sw1 or sw2);
+
+end Behavioral;
